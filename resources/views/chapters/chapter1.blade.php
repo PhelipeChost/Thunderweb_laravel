bed19f1a218e5b4e39e8f35b49f95c98e222fb69
Felipe Costa <125230782+PhelipeChost@users.noreply.github.com>
2023-10-25 23:19:04 -0300

    Separação do arquivo blade e css no primeiro capitulo (o restante será no desenvolvimento do projeto)

--- resources/views/chapters/chapter1.blade.php
+++ resources/views/chapters/chapter1.blade.php
@@ -8,141 +8,14 @@
 
         <link rel="icon" href="Imagens/home-logo.png">
 
+        <link rel="stylesheet" href="{{ url('Assets/css/chapter-number.css')}}">
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <style>
-        /* Usuario */
-
-        .navbar navbar-dark bg-dark {
-        position: fixed;
-        }
-
-        #logout {
-        font-family: sans-serif;
-        font-size: 15px;
-        font-weight: 700;
-        color: red;
-        padding: 5px 0px;
-        transition: all 0.5s;
-        text-decoration: none;
-        }
-
-        #logout:hover {
-        box-shadow: 0px 1px 0px white; 
-        }
-
-        /* Usuario */
-
-        /* MENU */
-
-        .container {
-        box-shadow: 0px 0px 1px white;
-        width: 1000px;
-        margin-top: 20px;
-        padding: 20px; 
-        }
-
-        li {
-
-        list-style: none;
-        display: inline-block;
-        padding: 45px;
-        }
-
-        #a-2 {
-        text-decoration: none;
-        color: red;
-        padding: 10px;
-        font-size: 20px;
-        font-weight: 700;
-        }
-
-        /* FIM MENU */
-
-
-
-        /* Páginas */
-
-            .capa-geral {
-                margin-top: 50px;
-                margin-bottom: 50px;
-                width: 10%;
-            }
-
-            @media (max-width: 576px) { 
-                .capa-geral {
-                width: 65%;
-            }
-            }
-
-            .publicidade {
-                background: black;
-                color: white;
-                text-align: center;
-                font-weight: 700;
-                padding: 5px;
-                font-size: 20px;
-            }
-
-            .paginas-manga {
-                width: 35%;
-            }
-
-            @media (max-width: 576px) { 
-                .paginas-manga {
-                width: 90%;
-            }
-            }
-
-            .nos-conheca {
-                width: 40%;
-            }
-
-            @media (max-width: 576px) { 
-                .nos-conheca {
-                width: 85%;
-            }
-            }
-
-        /* FIM Páginas */
-
-        /* Social */
-
-            #container-social {
-            background: black;
-            padding: 50px;
-            margin-top: 400px;
-            }
-
-            #copyright {
-            font-family: sans-serif;
-            text-align: center;
-            color: white;    
-            }
-
-            #social {
-            margin-left: auto;
-            margin-right: auto;
-            text-align: center;
-            margin-bottom: 50px;
-            }
-
-            #img-social {
-            padding: 10px;
-            border-radius: 35%;
-            }
-
-        </style>
-
     </head>
-    <body style="background: #2C2C2C">
-
-        
-        
+    <body>
         <!-- Logout -->
-
         <center>
             <nav class="navbar-dark bg-dark"  style="padding: 30px;">
                 <div class="container-fluid">
@@ -180,7 +53,7 @@
             <center><img class="img-fluid paginas-manga" src="{{ url('Assets/Manga/cap1/9.jpg') }}" alt="capítulp 1/página 8"></center><!-- página 10 -->
             <center><img class="img-fluid paginas-manga" src="{{ url('Assets/Manga/cap1/10.jpg') }}" alt="capítulp 1/página 8"></center><!-- página 11 -->
             <center><img class="img-fluid paginas-manga" src="{{ url('Assets/Manga/cap1/11.jpg') }}" alt="capítulp 1/página 8"></center><!-- página 12 -->
-        
+
         <div class="container-fluid" id="container-social">
             <p class="col" id="copyright">Copyight Jvellucy by</p>
             <p class="col" id="copyright">© 2021 - 2022 thunderweb.com.br</p>
@@ -189,12 +62,11 @@
                 <a href="https://www.instagram.com/thunderweb_/" target="_blank"><img src="Imagens/logo-insta.png"   class="img-fluid" id="img-social" width="50"></a>
             </div>
         </div>
-        <!-- FIM Social -->  
+        <!-- FIM Social -->
     </body>
 
     <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-    
+
      <script href="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-
 </html>
