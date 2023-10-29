7c557a273c0d80ee267d5efd3b3852d714e4e3fa
Felipe Costa <125230782+PhelipeChost@users.noreply.github.com>
2023-10-29 01:47:55 -0300

    Alteração no método de rotas - Rota logout e confirm-password porta qualquer tipo de rota ( any )

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/chapter{number}', [App\Http\Controllers\ChaptersallController::clas
 Route::get('/Peter-Enigma-dos-Senhos', [App\Http\Controllers\PartnershipallController::class, 'Peter']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::any('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
